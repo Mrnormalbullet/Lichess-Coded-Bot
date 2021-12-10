7d3ee7e3e3ba8edbff921330749ac4dcd676ec65
Mrnormalbullet <95895489+Mrnormalbullet@users.noreply.github.com>
2021-12-10 10:30:57 +0530

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -26,9 +26,7 @@ class Conversation:
             self.send_reply(line, "Waiting 60 seconds...")
         elif cmd == "name":
             name = game.me.name
-            self.send_reply(line, "{} using c++ and java codes running {} (lichess-bot v{}) on heroku server.".format(name, self.engine.name(), self.version))
-        elif cmd == "id":
-            self.send_reply(line, "ChessGreatPlayer")
+            self.send_reply(line, "{} Mostly Java.".format(name, self.engine.name(), self.version))
         elif cmd == "howto":
             self.send_reply(line, "How to run your own bot: Check out 'Lichess Bot API' or go to https://github.com/LichessBot-Coders/Lichess-Coded-Bot")
         elif cmd == "eval":
